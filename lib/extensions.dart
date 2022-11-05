22e919b4362f0e3d5078400deeba24ea1661a1e4
pierre <pierre@weebi.com>
2022-11-05 17:26:47 +0100

    - [x] déplacer la méthode d'import et export herder/contact dans weebi_models - [x] factoriser la fonction d'import

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -5,3 +5,7 @@ export 'extensions/next_id.dart';
 export 'extensions/sembast_store_extension.dart';
 export 'extensions/tickets_obs.dart';
 export 'extensions/tickets.dart';
+export 'extensions/line_of_articles.dart';
+export 'extensions/contacts.dart';
+export 'extensions/herders.dart';
+export 'extensions/import_data_w.dart';
---
+++ lib/extensions/import_data_w.dart
@@ -0,0 +1,114 @@
+import 'package:models_base/common.dart' show Address, StockUnit;
+import 'package:models_weebi/weebi_models.dart'
+    show HerderWeebi, Tristate, ContactWeebi, LineOfArticles, ArticleWeebi;
+
+extension ImportData on List<List<dynamic>> {
+  List<HerderWeebi> extractHerdersFromParsedExcel(int nextId, String shopUuid) {
+    final herdersList = <HerderWeebi>[];
+    var _nextId = nextId; // just in case..
+    for (var i = 1; i < length; i++) {
+      final now = DateTime.now();
+      final table = this[i];
+      final herder = HerderWeebi(
+        updateDate: now,
+        status: true,
+        statusUpdateDate: now,
+        id: _nextId,
+        bidon: _nextId,
+        shopId: shopUuid,
+        firstName: table[0] != null ? '${table[0]}' : '',
+        lastName: table[1] != null ? '${table[1]}' : '',
+        tel: table[2] != null ? '${table[2]}' : '',
+        mail: table[3] != null ? '${table[3]}' : '',
+        address: table[4] != null ? '${table[4]}' : '',
+        isWoman: table[5] ?? false,
+        category: table[6] != null ? '${table[6]}' : '',
+      );
+      herdersList.add(herder);
+      nextId++;
+    }
+    return herdersList;
+  }
+
+  List<ContactWeebi> extractContactsFromParsedExcel(
+      List<List<dynamic>> data, int nextLineId, String shopUuid) {
+    final contactsList = <ContactWeebi>[];
+    var nextId = nextLineId; // just in case..
+    for (var i = 1; i < data.length; i++) {
+      final now = DateTime.now();
+      final table = data[i];
+      final _address = Address.addressEmpty;
+      final newContact = ContactWeebi(
+        updateDate: now,
+        creationDate: now,
+        status: true,
+        statusUpdateDate: DateTime.now(),
+        id: nextId,
+        shopId: shopUuid,
+        tel: '',
+        mail: '',
+        category: '',
+        firstName: table[0] != null ? '${table[0]}' : '',
+        lastName: table[1] != null ? '${table[1]}' : '',
+        isWoman: table[5] != null && table[5] is String
+            ? Tristate.tryParse(table[5])
+            : Tristate.unknown,
+        address: _address, // make this a list
+      );
+      contactsList.add(newContact);
+      nextId++;
+    }
+    return contactsList;
+  }
+
+  List<LineOfArticles> extractLinesOfArticlesFromParsedExcel(
+      int nextLineId, String shopUuid) {
+    final linesList = <LineOfArticles>[];
+    var nextId = nextLineId; // just in case..
+    for (var i = 1; i < length; i++) {
+      final now = DateTime.now();
+      final table = this[i];
+      final _newArticle = ArticleWeebi(
+        creationDate: now,
+        updateDate: now,
+        shopUuid: shopUuid,
+        lineId: nextId,
+        id: 1,
+        fullName: table[0] != null ? '${table[0]}' : '',
+        price: table[1] != null
+            ? table[1].runtimeType == int
+                ? table[1]
+                : int.tryParse(table[1])
+            : 0,
+        cost: table[2] != null
+            ? table[2].runtimeType == int
+                ? table[2]
+                : int.tryParse(table[2])
+            : 0,
+        weight: 1.0,
+        articleCode: table[3] != null
+            ? table[3].runtimeType == int
+                ? table[3]
+                : int.tryParse(table[3])
+            : 0,
+        photo: '',
+      );
+      final newLine = LineOfArticles(
+        creationDate: now,
+        updateDate: now,
+        shopUuid: shopUuid,
+        id: nextId,
+        title: table[0] != null ? '${table[0]}' : '',
+        status: true,
+        statusUpdateDate: DateTime.now(),
+        stockUnit: StockUnit.unit,
+        categories: [''],
+        photo: '',
+        articles: [_newArticle],
+      );
+      linesList.add(newLine);
+      nextId++;
+    }
+    return linesList;
+  }
+}
