2de0ce0da7ee90de2173b53b0dbdaafc1664d12f
pierre <pierre@weebi.com>
2022-12-14 01:44:39 +0100

    mix

--- lib/abstractions.dart
+++ lib/abstractions.dart
@@ -2,3 +2,4 @@ library abstractions;
 
 // this lib is handy to separate mobx logic (business) from UI
 export 'src/models/abstractions/store_mobx.dart';
+export 'src/models/abstractions/filter.dart';
